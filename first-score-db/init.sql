00e67253b373d0782df8f75ada62ee55897da299
kunzgabriel <gabrielkunz2012@gmail.com>
2021-10-02 11:59:47 -0300

    fix: ajuste em criacao do banco de dados

--- first-score-db/init.sql
+++ first-score-db/init.sql
@@ -7,7 +7,7 @@ create table equipes (
     id int generated always as identity primary key,
     nome varchar(50) not null,
     id_game int not null,
-    constraint fk_game_to_equipe foreign key (id_game) references game (id)
+    constraint fk_games_to_equipes foreign key (id_game) references games (id)
 );
 
 create table jogadores (
@@ -21,8 +21,8 @@ create table equipe_jogadores (
     id int generated always as identity primary key,
     id_equipe int not null,
     id_jogador int not null,
-    1constraint fk_equipe_to_equipe_jogadores foreign key (id_equipe) references equipe (id),
-    constraint fk_jogador_to_equipe_jogadores foreign key (id_jogador) references jogador (id)
+    constraint fk_equipes_to_equipe_jogadores foreign key (id_equipe) references equipes (id),
+    constraint fk_jogadores_to_equipe_jogadores foreign key (id_jogador) references jogadores (id)
 );
 
 create table torneios (
@@ -32,8 +32,8 @@ create table torneios (
     data_final date,
     id_game int not null,
     equipe_vencedora int,
-    constraint fk_game_to_torneio foreign key (id_game) references game (id),
-    constraint fk_equipe_to_torneio foreign key (equipe_vencedora) references equipe (id)
+    constraint fk_games_to_torneios foreign key (id_game) references games (id),
+    constraint fk_equipes_to_torneios foreign key (equipe_vencedora) references equipes (id)
 );
 
 create table torneio_partidas (
@@ -45,7 +45,7 @@ create table torneio_partidas (
     pontos_equipe_1 int,
     equipe_2 int,
     pontos_equipe_2 int,
-    constraint fk_torneio_to_torneio_partidas foreign key (id_torneio) references torneio (id),
-    constraint fk_equipe1_to_torneio_partidas foreign key (equipe_1) references equipe (id),
-    constraint fk_equipe2_to_torneio_partidas foreign key (equipe_2) references equipe (id)
+    constraint fk_torneio_to_torneio_partidas foreign key (id_torneio) references torneios (id),
+    constraint fk_equipe1_to_torneio_partidas foreign key (equipe_1) references equipes (id),
+    constraint fk_equipe2_to_torneio_partidas foreign key (equipe_2) references equipes (id)
 );
